f3eb6b0b84a01d1d70edd7ba29db0c9c667dcd23
shatha200 <shathadalhoumy@gmail.com>
2024-09-05 21:59:50 +0100

    .

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -9,26 +9,30 @@ import gsap from 'gsap'
 import Image from "next/image";
 import Section from '@/components/Section';
 import Paragraph from '@/components/Paragraph';
+import Word from '@/components/Word';
+import Character from '@/components/Character';
+import Hero from '@/components/Hero';
 import { FloatingDock } from "@/components/ui/floating-dock";
-
+import Link from "next/link";
 // import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
-
+import { PlaceholdersAndVanishInput } from "@/components/ui/placeholders-and-vanish-input";
 // import HoverDevCards from '@/components/HoverDevCards';
 // import {projects} from '@/data';
 import Lenis from '@studio-freight/lenis';
 // import Card from '@/components/Card/index';
 // import Example from '@/components/HorizontalScrollCarousel';
 import { useScroll, useTransform } from 'framer-motion';
+import GradientButton from '@/components/GradientButton';
 import AuroraHero from '@/components/AuroraHero';
 import FeatureCard from "@/components/FeatureCard";
+import FlipLink from '@/components/RevealLinks';
 import VelocityText from '@/components/VelocityText';
-
-import Footer  from '@/components/Footer';
-import Title from '@/components/Title';
+import Header from '@/components/nav/Header';
 
 import { IconHome, IconCurrencyDollar, IconMessageCircle, IconMail, IconVideo, IconFileText, IconBook, IconClipboard } from '@tabler/icons-react';//   IconDollarSign,
 import { useRouter } from 'next/navigation';
-
+import Footer from '@/components/Footer';
+import Title from '@/components/Title'
 
 import {
   useMotionTemplate,
